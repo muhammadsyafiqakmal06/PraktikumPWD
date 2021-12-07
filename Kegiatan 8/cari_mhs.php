6fb57935a84b9c1a9cd7947f77bfc2e28a7c764a
muhammadsyafiqakmal06 <akmal.indo456@gmail.com>
2021-12-07 20:29:00 +0700

    Kegiatan 8

--- Kegiatan 8/cari_mhs.php
+++ Kegiatan 8/cari_mhs.php
@@ -1,7 +1,7 @@
 <?php 
 include 'koneksi.php';
 ?>
-<h3>Form Pencarian DATA KHS Dengan PHP </h3>
+<h3>Form Pencarian Dengan PHP MAHASISWA</h3>
 <form action="" method="get">
 <label>Cari :</label>
 <input type="text" name="cari">
@@ -16,19 +16,15 @@ echo "<b>Hasil pencarian : ".$cari."</b>";
 <table border="1">
 <tr>
 <th>No</th>
-<th>NIM</th>
 <th>Nama</th>
-<th>Kode MK</th>
-<th>Nama Matkul</th>
-<th>Nilai</th>
 </tr>
 <?php 
 if(isset($_GET['cari'])){
 $cari = $_GET['cari'];
-$sql="SELECT khs.nim, mahasiswa.nama, khs.kodeMK, khs.nama_matakuliah, khs.nilai FROM khs INNER JOIN mahasiswa ON khs.nim = mahasiswa.nim WHERE khs.nim like '%".$cari."%'";
+$sql="SELECT * FROM mahasiswa WHERE nama like'%".$cari."%'";
 $tampil = mysqli_query($con,$sql);
 }else{
-$sql="SELECT khs.nim, mahasiswa.nama, khs.kodeMK, khs.nama_matakuliah, khs.nilai FROM khs INNER JOIN mahasiswa ON khs.nim = mahasiswa.nim ";
+$sql="SELECT * FROM mahasiswa";
 $tampil = mysqli_query($con,$sql);
 }
 $no = 1;
@@ -36,11 +32,7 @@ while($r = mysqli_fetch_array($tampil)){
 ?>
 <tr>
 <td><?php echo $no++; ?></td>
-<td><?php echo $r['nim']; ?></td>
 <td><?php echo $r['nama']; ?></td>
-<td><?php echo $r['kodeMK']; ?></td>
-<td><?php echo $r['nama_matakuliah']; ?></td>
-<td><?php echo $r['nilai']; ?></td>
 </tr>
 <?php } ?>
 </table>
